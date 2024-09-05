490479aac4639f24675985e782ee1985ec85ea2c
songdoan96 <songdoan96@gmail.com>
2024-09-05 08:10:23 +0700

    kcs edit tab

--- resources/views/kcs/edit.blade.php
+++ resources/views/kcs/edit.blade.php
@@ -3,7 +3,7 @@
     {{-- <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
 @endpush
 @section('content')
-    <div class="flex justify-center items-center w-screen bg-black text-white overflow-x-hidden overflow-y-scroll pb-60">
+    <div class="flex justify-center items-center w-screen bg-black text-white overflow-x-hidden overflow-y-scroll pb-72">
         <div class="w-full h-full md:w-2/3 p-4">
             <a href="{{ route('kcs.dashboard') }}" class="flex items-center justify-center">
                 <img src="{{ asset('images/logo2.png') }}" alt="Logo" width="300">
