dce438dce55950a1d4bbc014f8171c441bd42c4a
songdoan96 <songdoan96@gmail.com>
2024-09-24 10:07:07 +0700

    api test

--- routes/api.php
+++ routes/api.php
@@ -36,6 +36,6 @@ Route::prefix("v1")->group(function () {
 });
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
