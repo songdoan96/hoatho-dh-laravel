db7e4c8026e34dbe4ae6d1b5538b9e12770bb03c
songdoan96 <songdoan96@gmail.com>
2024-10-12 15:18:12 +0700

    btp minus

--- resources/views/produce/edit-btp.blade.php
+++ resources/views/produce/edit-btp.blade.php
@@ -29,8 +29,7 @@
                 </div>
                 <div class="my-5 flex items-center gap-2">
                     <label for="btpNew" class="font-medium w-1/2">BTP cấp thêm</label>
-                    <input type="number" id="btpNew" name="btpNew" min="0"
-                        max="{{ $plan->sltacnghiep - $plan->btpcap }}"
+                    <input type="number" id="btpNew" name="btpNew" max="{{ $plan->sltacnghiep - $plan->btpcap }}"
                         class="flex-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5" />
                 </div>
                 <div class="flex justify-center gap-2">
